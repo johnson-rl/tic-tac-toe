de43f79856fbdc0e8ab11682d2168c5173e4869d
johnson-rl <johnson.rl@gmail.com>
2016-12-03 16:40:42 -0800

    Game debugged, some color added.  Next, test for winner, add winner page, let user select color scheme

--- js/app.js
+++ js/app.js
@@ -1,19 +1,47 @@
 console.log('javasctipt is loaded')
-var userTurn = 'X'
+var userTurn = 'X';
+var boxesX = [];
+var boxesO = [];
+
 function makeMove(event){
-  $(this).text(userTurn);
-  if(userTurn ==='O'){
-    userTurn='X';
-  } else {
-    userTurn='O';
+  var elementClass = ($(this).attr('class').split(' '));
+  var whichBox = elementClass.pop();
+  // console.log(whichBox);
+  console.log(elementClass);
+  // if (whichBox !="playedO"){
+  //   console.log("playedO working")
+  // }
+  // if (whichBox !="playedX"){
+  //   console.log("playedX working")
+  // }
+  if (((whichBox !="playedO")&&(whichBox !="playedX"))){
+    // debugger;
+    console.log(whichBox);
+    $(this).text(userTurn);
+    if(userTurn ==='O'){
+      boxesO.push(whichBox);
+      $(this).attr('class',elementClass.join(' ') + ' playedO');
+      userTurn='X';
+    } else {
+      boxesX.push(whichBox);
+      $(this).attr('class',elementClass.join(' ') + ' playedX');
+      userTurn='O';
+    }
+    // checkWinner();
+    console.log('the user took a turn')
   }
-  console.log('the user took a turn')
+}
+
+function checkWiner(){
+
 }
 
 function resetGame(event){
   $('.box').text('');
   console.log("The game has been reset");
   userTurn = 'X';
+  boxesX = [];
+  boxesO = [];
 }
 // wait for the DOM to finish loading
 $(document).ready(function() {
