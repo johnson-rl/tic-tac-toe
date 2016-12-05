1e8ae55e395e29a6338a1e2b24c5dba3d0603f80
johnson-rl <johnson.rl@gmail.com>
2016-12-04 23:25:39 -0800

    Fully functional clean code

--- js/app.js
+++ js/app.js
@@ -1,36 +1,26 @@
-console.log('javasctipt is loaded')
+console.log('javasctipt is loaded');
 var userTurn = 'X';
-var boxesX = ['X'];
+var boxesX = ['X'];//arrays that store played boxes for each player
 var boxesO = ['O'];
-var winners = [['one','two','three'],
+var winners = [['one','two','three'], //possible winning combos
               ['four','five','six'],
               ['seven','eight','nine'],
               ['one','five','nine'],
               ['three','five','seven'],
               ['one','four','seven'],
               ['two','five','eight'],
-              ['three','six','nine']]
-var playedX = ' playedgreen'
-var playedO = ' playedred'
-
+              ['three','six','nine']];
+var playedX = ' playedgreen';//player colors
+var playedO = ' playedred';
+//the next function takes a turn runs the checkwinner function for each player, aslo checks for cat's game
 function makeMove(event){
   var elementClass = ($(this).attr('class').split(' '));
-  var whichBox = elementClass.pop();
-  // console.log(whichBox);
-  console.log(elementClass);
-  // if (whichBox !="playedO"){
-  //   console.log("playedO working")
-  // }
-  // if (whichBox !="playedX"){
-  //   console.log("playedX working")
-  // }
+  var whichBox = elementClass.pop();//box number popped off, will be added to this player's array
   if ((((' '+whichBox) != playedO)&&((' '+whichBox) != playedX))){
-    // debugger;
-    console.log(whichBox);
     $(this).text(userTurn);
     if(userTurn ==='O'){
-      boxesO.push(whichBox);
-      $(this).attr('class',elementClass.join(' ') + playedO);
+      boxesO.push(whichBox);//box number added to user array
+      $(this).attr('class',elementClass.join(' ') + playedO);//color class added to $(this).  Changes background color to user color, but more importantly, prevents playing the same box twice.
       userTurn='X';
     } else {
       boxesX.push(whichBox);
@@ -40,20 +30,18 @@ function makeMove(event){
     checkWinner(boxesX);
     checkWinner(boxesO);
     if (boxesO.length+boxesX.length === 11){catsGame()}
-    $('.turn').text(userTurn)
-    console.log('the user took a turn')
+    $('.turn').text(userTurn);
   }
 }
 
-function catsGame(){
-  console.log("it's a cat's game");
+function catsGame(){//display's cat's game screen
   $('.starting-color').attr('class','cat-class');
   $('.container').attr('class','hidden');
   $('.cats').attr('class','cats');
   $('.turn-tracker').attr('class','hidden');
 }
 
-function checkWinner(player){
+function checkWinner(player){//checks winner arrays against player array for match
   for (var i=0;i<8;i++){
     var winTotal = 0;
     for (var j=0;j<3;j++){
@@ -67,8 +55,7 @@ function checkWinner(player){
   }
 }
 
-function declareWinner(winner){
-  console.log('winner');
+function declareWinner(winner){//displays winner screen
   $('.win').attr('class','show');
   $('.container').attr('class','hidden');
   $('.turn-tracker').attr('class','hidden');
@@ -77,62 +64,20 @@ function declareWinner(winner){
   } else {
     $('.starting-color').attr('class', playedO);
   }
-  // $('.winnerone').text('The Winner is');
   $('.winner').text(winner);
+  boxesX.pop();
 }
 
-function resetGame(event){
-  // $('.box').text('');
-  // console.log("The game has been reset");
-  // userTurn = 'X';
-  // boxesX = [];
-  // boxesO = [];
-  location.reload()
-}
-
-// function choosePlayer(player){
-//   $('#myModal').modal('show');
-//   $('#red').on('click', function(){player =' playedred'}); //not sure why I can't use function with arguments
-//   $('#blue').on('click', function(){player =' playedblue'});
-//   $('#pink').on('click', function(){player =' playedpink'});
-//   $('#green').on('click', function(){player =' playedgreen'});
-// }
-
-function playerSet(player,color){
-  return player = color;
-  console.log(playedX)
-}
-
-// wait for the DOM to finish loading
 $(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
-  console.log('so is jquery, and the html body is ready')
-  // $('#myModal').modal('toggle');
-  // if (!playedX){
-  //   choosePlayer(PlayerX);
-  // } else if (!playedO){
-  //   choosePlayer(played);
-  // } else {
-  //   $('#board').on('click','.box', makeMove);
-  //   $('.btn').on('click',resetGame);
-  // }
-  // choosePlayer(playedO);
-  // while(!playedO){
-  //     if (!playedX){
-      $('#myModal').modal('show');
-      $('#red').on('click', function(){playedX =' playedred'}); //not sure why I can't use function with arguments
-      $('#blue').on('click', function(){playedX =' playedblue'});
-      $('#pink').on('click', function(){playedX =' playedpink'});
-      $('#green').on('click', function(){playedX =' playedgreen'});
-    // } else {
-      // $('#myModal').modal('show');
-      $('#Ored').on('click', function(){playedO =' playedred'});
-      $('#Oblue').on('click', function(){playedO =' playedblue'});
-      $('#Opink').on('click', function(){playedO =' playedpink'});
-      $('#Ogreen').on('click', function(){playedO =' playedgreen'});
-  //   }
-  // }
-  $('#board').on('click','.box', makeMove);
-  $('.reset').on('click',resetGame)
+  $('#myModal').modal('show');
+  $('#red').on('click', function(){playedX =' playedred'}); //not sure why I can't use function with arguments
+  $('#blue').on('click', function(){playedX =' playedblue'});
+  $('#pink').on('click', function(){playedX =' playedpink'});
+  $('#green').on('click', function(){playedX =' playedgreen'});
+  $('#Ored').on('click', function(){playedO =' playedred'});
+  $('#Oblue').on('click', function(){playedO =' playedblue'});
+  $('#Opink').on('click', function(){playedO =' playedpink'});
+  $('#Ogreen').on('click', function(){playedO =' playedgreen'});
+  $('#board').on('click','.box', makeMove);//listens to board, makes move using specific box clicked as $(this)
+  $('.reset').on('click',function(){location.reload()})
 });
